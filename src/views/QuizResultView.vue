5ed548cebf019cb543b6cdddfdc04819db27996e
muhammad.i.fidatama <iilhamfidatama@gmail.com>
2023-09-04 16:20:22 +0700

    navigate to result view with pass data

---
+++ src/views/QuizResultView.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import {useRoute} from "vue-router";
+  import {ref, onBeforeMount} from "vue";
+
+  const route = useRoute()
+  const {correctAnswered, totalQuestions} = JSON.parse(route.params.data)
+
+  onBeforeMount(() => {
+    console.log(route.params)
+  })
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ correctAnswered }}/{{ totalQuestions }}</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
